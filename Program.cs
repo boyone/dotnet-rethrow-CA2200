232636c9ea50d3716a606818d6c05a339db9db91
boyone <boyone.g@gmail.com>
2023-01-27 11:01:03 +0700

    Init project

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Program program = new Program();
+        program.CatchException();
+    }
+
+    void CatchException()
+    {
+        try
+        {
+            CatchAndRethrowExplicitly();
+        }
+        catch (ArithmeticException e)
+        {
+            Console.WriteLine("Explicitly specified:{0}{1}",
+               Environment.NewLine, e.StackTrace);
+        }
+
+        try
+        {
+            CatchAndRethrowImplicitly();
+        }
+        catch (ArithmeticException e)
+        {
+            Console.WriteLine("{0}Implicitly specified:{0}{1}",
+               Environment.NewLine, e.StackTrace);
+        }
+    }
+
+    void CatchAndRethrowExplicitly()
+    {
+        try
+        {
+            ThrowException();
+        }
+        catch (ArithmeticException e)
+        {
+            // Violates the rule.
+            throw e;
+        }
+    }
+
+    void CatchAndRethrowImplicitly()
+    {
+        try
+        {
+            ThrowException();
+        }
+        catch (ArithmeticException)
+        {
+            // Satisfies the rule.
+            throw;
+        }
+    }
+
+    void ThrowException()
+    {
+        throw new ArithmeticException("illegal expression");
+    }
+}
